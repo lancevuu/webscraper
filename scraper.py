f0abf510d092871b5b11dbfb89314b0f56a25c2a
Lance Vu <lancev@uci.edu>
2025-01-07 11:18:55 -0800

    added base code for scrape

--- scraper.py
+++ scraper.py
@@ -1,1 +1,15 @@
-#testing
+import requests
+from bs4 import BeautifulSoup
+
+def scrape_data(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        titles = [title.text for title in soup.find_all('h2')]
+        return titles
+    else:
+        return []
+
+if __name__ == "__main__":
+    url = "https://example.com"
+    print(scrape_data(url))
